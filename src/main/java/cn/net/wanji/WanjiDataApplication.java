374e270b5a60edc9647eca43993daf6f746932dd
zhaoling <zhaoling@wanji.net.cn>
2024-06-18 08:13:52 +0800

    初始化项目
    Change-Id: Id521798295b5ea82296aad93acda543434c62d95

---
+++ src/main/java/cn/net/wanji/WanjiDataApplication.java
@@ -0,0 +1,17 @@
+package cn.net.wanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+/**
+ * 启动程序!
+ *
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class WanjiDataApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(WanjiDataApplication.class, args);
+    }
+}
